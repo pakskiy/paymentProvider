61964fc5ebe3c9846cc95d820a9556e845b90656
pakskiy <pakskiy@gmail.com>
2024-02-13 23:49:13 +0500

    Refactoring

--- src/main/java/com/pakskiy/paymentProvider/service/ClearingService.java
+++ src/main/java/com/pakskiy/paymentProvider/service/ClearingService.java
@@ -1,6 +1,7 @@
 package com.pakskiy.paymentProvider.service;
 
 import com.pakskiy.paymentProvider.dto.TransactionStatus;
+import com.pakskiy.paymentProvider.entity.AccountEntity;
 import com.pakskiy.paymentProvider.entity.TransactionEntity;
 import com.pakskiy.paymentProvider.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,7 @@ import reactor.core.scheduler.Schedulers;
 public class ClearingService {
     private final PaymentRepository paymentRepository;
     private final NotificationService notificationService;
+    private final AccountService accountService;
     public Mono<Void> clear() {
         return paymentRepository.findAllByOrderByCreatedAt()
                 .groupBy(TransactionEntity::getMerchantId)
@@ -25,9 +27,28 @@ public class ClearingService {
                 .runOn(Schedulers.parallel()).flatMap(this::clearTransaction).then();
     }
 
-    private Publisher<?> clearTransaction(GroupedFlux<Long, TransactionEntity> el) {
+    /*
+    * 1. Check if account exist
+    * 2. Check if transaction amount is equals or less when account deposit
+    * 3. If yes, then need minus amount from deposit and update account and transaction
+    * 4. If no, then need fail and update transaction
+    * 5. Try to send notification to user by notification_url
+    */
+    private Publisher<Void> clearTransaction(GroupedFlux<Long, TransactionEntity> el) {
         return el.map(transactionEntity -> {
             log.info("Transaction data {}", transactionEntity);
+
+            Mono<AccountEntity> accountEntity = accountService.getByMerchantId(transactionEntity.getMerchantId())
+                    .switchIfEmpty(Mono.error(new RuntimeException("Account not founded")));
+
+            Mono<Long> amount = accountEntity.map(res -> {
+                if(res.getDepositAmount() <= transactionEntity.getAmount()) {
+                    return res.getDepositAmount() - transactionEntity.getAmount();
+                }
+                return 0L;
+            });
+
+
             return transactionEntity;
         }).then();
     }
